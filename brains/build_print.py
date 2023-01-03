74bc32d7449b958615852456159ef899a8acd0de
Fedor Kuruts <mabuteec@gmail.com>
2023-01-03 20:27:41 +0600

    glue parcer

---
+++ brains/build_print.py
@@ -0,0 +1,76 @@
+from config import files
+from build_data import build_data
+from fuzzywuzzy import fuzz
+
+
+def build_person_print(folder,name):
+    BigData = build_data(folder, files)
+    for key in list(BigData.racers_initials.keys()):
+        ratio = fuzz.ratio(key.lower(), name.lower())
+        if ratio >= 75:
+            racer_key_name = key
+            break
+    INC = BigData.racers_initials[racer_key_name]
+    place = BigData.score[INC]
+    lap_time = BigData.time_lap[INC]
+    command = BigData.racers_info[INC][1]
+    print(str(place) + ". ", racer_key_name + " " + command, lap_time,sep="|")
+
+
+def build_place_print(place):
+    if len(str(place)) < 2:
+        return str(place) + ". "
+    else:
+        return str(place) +"."
+
+
+def build_total_report(folder):
+
+    BigData = build_data(folder, files)
+    score = BigData.score
+    fifteen = 0
+    report = []
+    for INC, value in score.items():
+        place = value
+        racer_name = BigData.racers_info[INC][0]
+        command = BigData.racers_info[INC][1]
+        lap_time = BigData.time_lap[INC]
+        string = (place, racer_name, command, lap_time)
+        report.append(string)
+    return report
+
+def check_max_string_length(report):
+    max_length = 0
+    for string in report :
+        if len(string[1]+string[2]) > max_length:
+            max_length = len(string[1]+string[2])
+    return max_length
+
+
+def build_total_print(folder,reverse):
+    print("reverse type: ",reverse)
+    report=build_total_report(folder)
+    max_length_name = check_max_string_length(report)
+    counter = 0
+    for string in report:
+        place = build_place_print(string[0])
+        name_and_command = string[1] +" "+ string[2] +(max_length_name-len(string[1]+string[2]))*" "
+        lap_time = string[3]
+        print(place, name_and_command, lap_time, sep="|")
+        if counter == 14:
+            print("-"*(len(place+str(lap_time))+max_length_name+3))
+        counter +=1
+
+
+
+
+if __name__ == "__main__":
+    build_person_print("Kimi Raikkenen")
+    build_total_print("storage")
+
+
+
+
+
+
+
---
+++ brains/start.py
@@ -0,0 +1,31 @@
+import argparse
+
+from build_print import build_person_print, build_total_print
+
+
+def use_parser():
+    parser = argparse.ArgumentParser(description="Monaco Racing Task",
+                                     epilog="I hope it will be funny")
+    parser.add_argument('--files', type=str, required=True, help="Enter your folder path")
+    group = parser.add_mutually_exclusive_group(required = True)
+    group.add_argument('--asc', help="direct order", action="store_true")
+    group.add_argument('--dasc', help="undirect order", action="store_true")
+    group.add_argument('--driver', help="driver name", type=str, default=None)
+    args = parser.parse_args()
+    print(args)
+    return args
+
+
+def run():
+    args = use_parser()
+    folder=args.files
+    if args.driver:
+         build_person_print(folder, args.driver)
+    elif args.asc:
+         build_total_print(folder,args.asc)
+    elif args.dasc:
+         build_total_print(folder,args.dasc)
+
+if __name__ == "__main__":
+    run()
+
