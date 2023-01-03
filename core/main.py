5399628c40d8466a18dce92913c0389ce215f77a
Fedor Kuruts <mabuteec@gmail.com>
2023-01-03 19:05:27 +0600

    parcer done

--- core/main.py
+++ core/main.py
@@ -4,14 +4,15 @@ import argparse
 def use_parser():
     parser = argparse.ArgumentParser(description="Monaco Racing Task",
                                      epilog="I hope it will be funny")
-    parser.add_argument('--files', type=str, default=None, help="Enter your folder path")
-    parser.add_argument('--asc', type=int, default=0, help="direct order")
-    parser.add_argument('-desc', type=int, default=1, help="reverse order")
-    parser.add_argument('--driver', type=str, default=None, help="Enter racer name")
-    parser.add_argument('move', choices=['--rock', '--paper', '--scissors'], default=None)
+    parser.add_argument('--files', type=str, required=True, help="Enter your folder path")
+    group = parser.add_mutually_exclusive_group(required = False)
+    group.add_argument('--asc', help="direct order", action="store_true")
+    group.add_argument('--dasc',help="undirect order", action="store_true")
+    group.add_argument('--driver',help="driver name")
+
 
     args = parser.parse_args()
-    print(args)
+
     return args
 
 
